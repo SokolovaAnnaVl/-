a5b543bc286cad1116932e56fed6671fdced2e18
Sokolova  Anna <124304899+SokolovaAnnaVl@users.noreply.github.com>
2023-02-14 02:20:35 +0800

    ДЗ семинар_3

---
+++ Задача_19/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine()!);
+int a1 = N / 10000;
+int a2 = N % 10;
+int b1 = N / 1000;
+int b2 = N % 100;
+
+if (a1 == a2)
+{
+    Console.Write("Yes");
+}
+
+else
+{
+    if (b1 == b2)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+}
---
+++ Задача_21/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними
+//  в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.Write("Введите координату первого числа, ось X: ");
+double Ax = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координату первого числа, ось Y: ");
+double Ay = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координату первого числа, ось Z: ");
+double Az = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Введите координату второго числа, ось X: ");
+double Bx = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координату второго числа, ось Y: ");
+double By = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координату второго числа, ось Z: ");
+double Bz = Convert.ToDouble(Console.ReadLine()!);
+
+double C = Math.Pow((Ax - Bx), 2) + 
+Math.Pow((Ay - By), 2) + Math.Pow((Az - Bz), 2);
+double R = Math.Sqrt(C);
+
+Console.WriteLine($"Расстояние между точками в 3D пространстве {R:f2}");
